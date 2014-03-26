e8d43fc848e9d21e2cfab80113167eae0615243d
unfly <unflys@gmail.com>
2014-03-26 20:07:02 +0800

    modifiedviews

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title){"Ruby on Rails Tutorial Sample App"}
+	
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App|Home')
+			expect(page).to have_title("#{base_title}|Home")
 		end
 	end
 
@@ -13,6 +16,7 @@ describe "StaticPages" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
+			expect(page).to have_title("#{base_title}|Help")
 		end
 	end
 
@@ -20,6 +24,7 @@ describe "StaticPages" do
 		it "should have the content 'About'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About')
+			expect(page).to have_title("#{base_title}|About")
 		end
 	end
 end
